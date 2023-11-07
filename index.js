56d61ab16bbf39a5f0442912c7cb9c1e76a409b6
Joshua Lane <67156874+lanejm@users.noreply.github.com>
2023-11-07 15:15:16 -0500

    async function

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 const fs = require('fs')
-const { text } = require('stream/consumers')
 
-const textInput = fs.readFileSync('./txt/input.txt', 'utf-8')
-console.log(textInput)
+//blocking, synchronous way
+// const { text } = require('stream/consumers')
 
-const textOutput = `this is what we know about the avocado: ${textInput}.\nCreated on ${Date.now()}` 
-fs.writeFileSync('./txt/output.txt', textOutput)
-console.log('File written')
+// const textInput = fs.readFileSync('./txt/input.txt', 'utf-8')
+// console.log(textInput)
+
+// const textOutput = `this is what we know about the avocado: ${textInput}.\nCreated on ${Date.now()}` 
+// fs.writeFileSync('./txt/output.txt', textOutput)
+// console.log('File written')
+
+//non-blocking, asynchronous way
+fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
+    fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
+        console.log(data2);
+        fs.readFile('./txt/append.txt', 'utf-8', (err, data3) => {
+            console.log(data3)
+
+            fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', err => {
+                console.log('Your file has been written')
+            })
+        })
+    })
+})
+console.log('Will read file!')
