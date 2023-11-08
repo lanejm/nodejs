3920fdf07382402f7759725c5b0f5bde3954ff31
Joshua Lane <67156874+lanejm@users.noreply.github.com>
2023-11-08 08:02:00 -0500

    sws

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const fs = require('fs')
 const http = require('http')
 
+//////////////////////////////////////////////////////
+//files
+
 //blocking, synchronous way
 // const { text } = require('stream/consumers')
 
@@ -12,16 +15,26 @@ const http = require('http')
 // console.log('File written')
 
 //non-blocking, asynchronous way
-fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
-    fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
-        console.log(data2);
-        fs.readFile('./txt/append.txt', 'utf-8', (err, data3) => {
-            console.log(data3)
+// fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
+//     fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
+//         console.log(data2);
+//         fs.readFile('./txt/append.txt', 'utf-8', (err, data3) => {
+//             console.log(data3)
 
-            fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', err => {
-                console.log('Your file has been written')
-            })
-        })
-    })
+//             fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', err => {
+//                 console.log('Your file has been written')
+//             })
+//         })
+//     })
+// })
+// console.log('Will read file!')
+
+/////////////////////////////////////////
+//server
+const server = http.createServer((req, res) => {
+    res.end('Hello from the server!')
 })
-console.log('Will read file!')
+
+server.listen(8000, '127.0.0.1', () => {
+    console.log(`Listening at port 8000`)
+})
