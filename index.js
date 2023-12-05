9662d747a189ac5c053a7ba944247d46679712f8
Joshua Lane <67156874+lanejm@users.noreply.github.com>
2023-12-05 13:46:15 -0500

    dynamic cards

--- index.js
+++ index.js
@@ -32,9 +32,22 @@ const url = require('url')
 
 /////////////////////////////////////////
 //server
-const tempOverview = fs.readFileSync('./dev-data/templates/template-overview.html', 'utf-8')
-const tempCard = fs.readFileSync('./dev-data/templates/template-card.html', 'utf-8')
-const tempProduct = fs.readFileSync('./dev-data/templates/template-product.html', 'utf-8')
+const replaceTemplate = (temp, product) => {
+    let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName)
+    output = output.replace(/{%IMAGE%}/g, product.image)
+    output = output.replace(/{%PRICE%}/g, product.price)
+    output = output.replace(/{%FROM%}/g, product.from)
+    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients)
+    output = output.replace(/{%QUANTITY%}/g, product.quantity)
+    output = output.replace(/{%DESCRIPTION%}/g, product.description)
+    output = output.replace(/{%ID%}/g, product.id)
+
+    if(!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic')
+    return output
+}
+const tempOverview = fs.readFileSync('./templates/template-overview.html', 'utf-8')
+const tempCard = fs.readFileSync('./templates/template-card.html', 'utf-8')
+const tempProduct = fs.readFileSync('./templates/template-product.html', 'utf-8')
 const data = fs.readFileSync('./dev-data/data.json', 'utf-8') //read file synchronously
 const dataObject = JSON.parse(data)
 
@@ -46,8 +59,11 @@ const server = http.createServer((req, res) => {
 //Overview page
 
     if(pathName === '/' || pathName === '/overview'){
+        res.writeHead(200, {'Content-Type': 'text/html'})
 
-        res.end("this is the overview")
+        const cardsHtml = dataObject.map(el => replaceTemplate(tempCard, el)).join('')
+        const output = tempOverview.replace('{%PRODUCT_CARDS%', cardsHtml)
+        res.end(output)
 
     //Product page
     } else if(pathName === '/product'){
